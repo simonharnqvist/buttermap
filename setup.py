5129f642aac9af36d9a9e95f4d365b541cc0daba
Simon Harnqvist <simon.harnqvist@ed.ac.uk>
2023-11-09 10:43:06 +0000

    updated dependencies

--- setup.py
+++ setup.py
@@ -3,9 +3,7 @@ setup(
     packages=["buttermap"],
     name='buttermap',
     version='0.0.1',
-    install_requires=[
-        "joblib",
-        "ruffus"],
+    install_requires=["ruffus"],
     entry_points={
         'console_scripts': [
             'buttermap=buttermap.buttermap:main'
