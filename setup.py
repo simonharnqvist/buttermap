27b79312da54d4641fabe69fe15dbf86bf3a63c4
Simon Harnqvist <simon.harnqvist@ed.ac.uk>
2023-12-15 12:19:47 +0000

    build 0.0.2

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 setup(
     packages=["buttermap"],
     name='buttermap',
-    version='0.0.1',
+    version='0.0.2',
     install_requires=["ruffus"],
     entry_points={
         'console_scripts': [
